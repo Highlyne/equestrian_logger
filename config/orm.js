623000d0ec2146a31a218070e1b270e9a882a579
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2018-01-23 00:16:59 -0500

    Displaying all ponies

--- config/orm.js
+++ config/orm.js
@@ -12,19 +12,10 @@ var orm= {
         });
     },
 
-    createOne: function(val, cb) {
-        var queryString = "INSERT INTO ponies VALUES (?)"
-        connection.query(queryString, [val], function(err,result) {
-        if (err) {
-      // If an error occurred, send a generic server faliure
-      return res.status(500).end();
-    }
-        cb(result);
-        });
-    },
+    
     updateOne: function(colToInsert, condition, cb) {
         var queryString = "UPDATE ponies SET pony_name = ? WHERE id=? "
-        connection.query(queryString, [newPonyName, ponyID], function(err,result) {
+        connection.query(queryString, [req.body.ponyName, req.params.id], function(err,result) {
             if (err) throw err;
         cb(results);
     })
--- controllers/ponyController.js
+++ controllers/ponyController.js
@@ -1,16 +1,16 @@
-
 // Requiring the Express package
 var express = require("express");
+var connection = require("../config/connections.js");
 
 // Creating the `router` for the app,
 var router = express.Router();
 
-// Import the model (pony.js) to use its database functions.
-var myPony = require("../models/pony.js");
+// Import the orm to use its database functions.
+var orm = require("../config/orm.js");
 
 // Create all the routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-    myPony.all(function(data) {
+    orm.readAll(function(data) {
       var hbsObject = {
         ponies: data
       };
@@ -19,34 +19,29 @@ router.get("/", function(req, res) {
     });
   });
   
-  router.post("/api/ponies", function(req, res) {
-    myPony.create(["name"], [req.body.name], function(result) {
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    });
-  });
+router.post("/add", function(req, res) {
+    connection.query("INSERT INTO ponies (pony_name) VALUES (?);", [req.body.newPony], function(err, results) {
+        if (err) throw err;
+        console.log("here is pony name: " +req.body.newPony);
+        res.redirect("/");
+    })
   
-  router.put("/api/ponies/:id", function(req, res) {
-    var condition = "id = " + req.params.id;
+      });
   
-    console.log("condition: ", condition);
-  
-    pony.update(
-      {
-        ride: req.body.ride
-      },
-      condition,
-      function(result) {
-        if (result.changedRows === 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        }
-        res.status(200).end();
-  
-      }
-    );
-  });
-  
+router.put("/update", function(req, res) {
+    connection.query("UPDATE ponies SET pony_name = ? WHERE id = ?", [req.body.newPonyNM, req.body.id], function(err,results) {
+        if (err) throw err;
+        console.log("Update: " + res);
+        res.redirect("/");
+    })
+});
+
+router.put("/ride", function(req, res){
+    connection.query("UPDATE ponies SET ride = 1 WHERE id = ?", [req.body.data-id], function(err, results) {
+        if (err) throw err;
+        res.redirect("/");
+    })
+});
   // Export routes for server.js to use.
   module.exports = router;
   
